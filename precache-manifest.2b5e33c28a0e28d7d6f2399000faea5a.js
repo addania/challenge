4602204c4f5078f005fc8b3b58c006cad0efdc7f
addania <mirka.schw@gmail.com>
2019-12-09 19:54:54 +0100

    Updates

---
+++ precache-manifest.2b5e33c28a0e28d7d6f2399000faea5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dc25a474a54bbfb1d75c708b3632f6",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "b7cc2bcffd2a87014189",
+    "url": "/challenge/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "395614dabfbdfe53ffe8",
+    "url": "/challenge/static/css/main.d599bf61.chunk.css"
+  },
+  {
+    "revision": "b7cc2bcffd2a87014189",
+    "url": "/challenge/static/js/2.664e66f7.chunk.js"
+  },
+  {
+    "revision": "bd58a53ea30c71d23a62143160dc052c",
+    "url": "/challenge/static/js/2.664e66f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "395614dabfbdfe53ffe8",
+    "url": "/challenge/static/js/main.ad35aa08.chunk.js"
+  },
+  {
+    "revision": "8ca62c94697f18a2fd0e",
+    "url": "/challenge/static/js/runtime-main.ecbe9631.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/challenge/precache-manifest.54b5069e40739f997f4c51719b594579.js"
+  "/challenge/precache-manifest.2b5e33c28a0e28d7d6f2399000faea5a.js"
 );
 
 self.addEventListener('message', (event) => {
