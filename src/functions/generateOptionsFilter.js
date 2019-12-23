cc556a2072211e911e1a6ad9b1e8a5a28387bb84
addania <mirka.schw@gmail.com>
2019-12-23 19:30:13 +0100

    refactoring generateOptionsFilter, removing for loop, let and mutable method, reducing rows of code

--- src/functions/generateOptionsFilter.js
+++ src/functions/generateOptionsFilter.js
@@ -1,19 +1,12 @@
 export const generateOptionsFilter = (inputItem, inputDataSet) => {
   // Receives input data and input item (based on filters) and dynamically generated options to be used for Dropdown component.
-  let uniqueValues = [];
-  for (let entry = 0; entry < inputDataSet.length; entry++) {
-    if (!uniqueValues.includes(inputDataSet[entry][inputItem])) {
-      uniqueValues.push(inputDataSet[entry][inputItem]);
-    }
-  }
-  let newOptions = [];
-  for (let optionItem = 0; optionItem < uniqueValues.length; optionItem++) {
-    let optionRow = {
-      key: uniqueValues[optionItem],
-      text: uniqueValues[optionItem],
-      value: uniqueValues[optionItem]
+  const uniqueValues = [...new Set(inputDataSet.map(item => item[inputItem]))];
+  const newOptions = uniqueValues.map(item => {
+    return {
+      key: item,
+      text: item,
+      value: item
     };
-    newOptions.push(optionRow);
-  }
+  });
   return newOptions;
 };
--- src/tests/generateOptionsFilter.test.js
+++ src/tests/generateOptionsFilter.test.js
@@ -62,4 +62,32 @@ test("generate Options Filter", () => {
     },
     { key: "B2B - Leads", text: "B2B - Leads", value: "B2B - Leads" }
   ]);
+  expect(
+    generateOptionsFilter("Campaign", [
+      {
+        Date: "01. Jan",
+        Datasource: "Facebook Ads",
+        Campaign: "Like Ads",
+        Clicks: 274,
+        Impressions: 1979
+      },
+      {
+        Date: "01. Jan",
+        Datasource: "Facebook Ads",
+        Campaign: "Like Ads",
+        Clicks: 10245,
+        Impressions: 764627
+      },
+      {
+        Date: "01. Jan",
+        Datasource: "Google Adwords",
+        Campaign: "B2B - Leads",
+        Clicks: 7,
+        Impressions: 444
+      }
+    ])
+  ).toStrictEqual([
+    { key: "Like Ads", text: "Like Ads", value: "Like Ads" },
+    { key: "B2B - Leads", text: "B2B - Leads", value: "B2B - Leads" }
+  ]);
 });
