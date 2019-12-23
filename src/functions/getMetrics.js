22324a2b394c152efd8369ddde3e89325baf8469
addania <mirka.schw@gmail.com>
2019-12-23 23:01:02 +0100

    excluding lodash in favour of native ES6 typeOf for getMetrics

--- src/functions/getMetrics.js
+++ src/functions/getMetrics.js
@@ -1,8 +1,6 @@
-import _ from "lodash";
-
 export const getMetrics = (inputData, inputColumns) => {
   // Receives an array of objects as inputData and array of unique keys as inputColumns.
   // Outputs array of column names which holds numeric values.
-  const output = inputColumns.filter(col => _.isNumber(inputData[col]));
+  const output = inputColumns.filter(col => typeof inputData[col] === "number");
   return output;
 };
