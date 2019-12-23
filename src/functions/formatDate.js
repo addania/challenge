f8e18661e7f649c8077afeff86c96c533b426cc3
addania <mirka.schw@gmail.com>
2019-12-23 12:41:05 +0100

    refactoring formatDate to remove for loop, let and mutational methods

--- src/functions/formatDate.js
+++ src/functions/formatDate.js
@@ -2,13 +2,14 @@ const moment = require("moment");
 
 export const formatDate = input => {
   // Receives an array of objects as input and formats date entries into a "DD. MMM" format. Outputs data as "dataWithDate".
-  const formattedDateArray = [];
-  for (let row = 0; row < input.length; row++) {
-    const entry = { ...input[row] };
-    const oldDate = input[row].Date;
+
+  const convertDate = row => {
+    const entry = { ...row };
+    const oldDate = row.Date;
     const dateFormatted = moment(oldDate, "DD.MM.YYYY").format("DD. MMM");
     entry.Date = dateFormatted;
-    formattedDateArray.push(entry);
-  }
+    return entry;
+  };
+  const formattedDateArray = input.map(convertDate);
   return formattedDateArray;
 };
