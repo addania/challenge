a0ce126262c9e0f46b0d84f05775932dc4f89767
addania <mirka.schw@gmail.com>
2019-12-23 19:55:49 +0100

    refactoring filterHelperFunction to remove for loop, let and .pushh method

--- src/functions/filterHelperFunction.js
+++ src/functions/filterHelperFunction.js
@@ -3,18 +3,16 @@ import { Dropdown } from "semantic-ui-react";
 import { generateOptionsFilter } from "./generateOptionsFilter";
 
 export const filterHelperFunction = (filterColumns, onChange, dataSet) => {
-  const columns = [];
-
-  for (let item = 0; item < filterColumns.length; item++) {
-    const genOptions = generateOptionsFilter(filterColumns[item], dataSet);
-    columns.push(
+  const getColumns = item => {
+    const genOptions = generateOptionsFilter(item, dataSet);
+    return (
       <div key={item}>
         <p style={{ textAlign: "left", color: "#828282", marginTop: "20px" }}>
-          {filterColumns[item]}
+          {item}
         </p>
         <Dropdown
           key={item}
-          placeholder={filterColumns[item]}
+          placeholder={item}
           fluid
           multiple
           selection
@@ -24,7 +22,7 @@ export const filterHelperFunction = (filterColumns, onChange, dataSet) => {
         />
       </div>
     );
-  }
-
+  };
+  const columns = filterColumns.map(getColumns);
   return columns;
 };
