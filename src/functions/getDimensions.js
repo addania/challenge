0091d9db9b8cd011cf036fa903793b856a113c40
addania <mirka.schw@gmail.com>
2019-12-23 22:58:12 +0100

    excluding lodash in favour of native ES6 typeOf for getDimensions

--- src/functions/getDimensions.js
+++ src/functions/getDimensions.js
@@ -1,8 +1,6 @@
-import _ from "lodash";
-
 export const getDimensions = (inputData, inputColumns) => {
   // Receives an array of objects as inputData and array of unique keys as inputColumns.
   // Outputs array of column names which holds string values (excluding date formats).
-  const output = inputColumns.filter(col => _.isString(inputData[col]));
+  const output = inputColumns.filter(col => typeof inputData[col] === "string");
   return output;
 };
