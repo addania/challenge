ed717cdf5643b07f3047199e37521f6c79704737
addania <mirka.schw@gmail.com>
2019-12-23 21:34:59 +0100

    refactoring calculateAggregates function, removing for loop, let and mutation

--- src/functions/calculateAggregates.js
+++ src/functions/calculateAggregates.js
@@ -7,26 +7,17 @@ export const calculateAggregates = array => {
   // Third element of the output array is array of unique dates.
   const groupByDate = _.groupBy(array, "Date");
   const uniqueDates = _.keys(groupByDate);
-  let impressionsArray = [];
-  let clicksArray = [];
-
-  for (let dayEntry = 0; dayEntry < uniqueDates.length; dayEntry++) {
-    let totalImpressions = 0;
-    let totalClicks = 0;
-
-    for (
-      let dataRow = 0;
-      dataRow < groupByDate[uniqueDates[dayEntry]].length;
-      dataRow++
-    ) {
-      totalImpressions =
-        totalImpressions +
-        groupByDate[uniqueDates[dayEntry]][dataRow].Impressions;
-      totalClicks =
-        totalClicks + groupByDate[uniqueDates[dayEntry]][dataRow].Clicks;
-    }
-    impressionsArray.push(totalImpressions);
-    clicksArray.push(totalClicks);
-  }
+  const impressionsArray = uniqueDates.map(item => {
+    const sumImp = groupByDate[item].reduce((acc, obj) => {
+      return acc + obj.Impressions;
+    }, 0);
+    return sumImp;
+  });
+  const clicksArray = uniqueDates.map(item => {
+    const sumClick = groupByDate[item].reduce((acc, obj) => {
+      return acc + obj.Clicks;
+    }, 0);
+    return sumClick;
+  });
   return [impressionsArray, clicksArray, uniqueDates];
 };
