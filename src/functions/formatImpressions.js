b8817f52659c15f116d34383203cc988993ebe56
addania <mirka.schw@gmail.com>
2019-12-23 10:38:12 +0100

    refactoring formatImpressions function to remove for loop and let

--- src/functions/formatImpressions.js
+++ src/functions/formatImpressions.js
@@ -1,12 +1,15 @@
 export const formatImpressions = input => {
   // Receives an array of objects as inputs and substitutes empty or null impressions with 0.
-  for (let row = 0; row < input.length; row++) {
-    if (!input[row].Impressions) {
-      input[row].Impressions = 0;
+
+  const formatNumbers = row => {
+    if (!row.Impressions) {
+      row.Impressions = 0;
     }
-    if (!input[row].Clicks) {
-      input[row].Clicks = 0;
+    if (!row.Clicks) {
+      row.Clicks = 0;
     }
-  }
-  return input;
+    return row;
+  };
+  const output = input.map(formatNumbers);
+  return output;
 };
