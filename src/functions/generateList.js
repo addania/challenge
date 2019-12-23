eda65c3dd68ff8f6827a583474c3154d719a50bf
addania <mirka.schw@gmail.com>
2019-12-23 12:24:11 +0100

    refactor getMetrics and getDimensions, removing for loop and let, simplifying and shorteining code

--- src/functions/generateList.js
+++ src/functions/generateList.js
@@ -1,16 +1,17 @@
 export const generateList = filterItem => {
   // Receives an array of filter values. Generates a string of maximum 3 values
   // if multiple filter values are selected.
+  //console.log("filterItem",filterItem);
   let string = "";
-  let newString;
   for (let entry = 0; entry < filterItem.length && entry < 3; entry++) {
     string = string + JSON.stringify(filterItem[entry]) + " and ";
   }
-
+  //console.log("string", string);
   if (filterItem.length <= 3) {
-    newString = string.slice(0, -4);
+    const newString = string.slice(0, -4);
+    return newString;
   } else {
-    newString = string.slice(0, -5) + ", etc.";
+    const newString = string.slice(0, -5) + ", etc.";
+    return newString;
   }
-  return newString;
 };
--- src/functions/getMetrics.js
+++ src/functions/getMetrics.js
@@ -3,12 +3,6 @@ import _ from "lodash";
 export const getMetrics = (inputData, inputColumns) => {
   // Receives an array of objects as inputData and array of unique keys as inputColumns.
   // Outputs array of column names which holds numeric values.
-  const output = [];
-  for (let item = 0; item < inputColumns.length; item++) {
-    const col = inputColumns[item];
-    if (_.isNumber(inputData[col])) {
-      output.push(col);
-    }
-  }
+  const output = inputColumns.filter(col => _.isNumber(inputData[col]));
   return output;
 };
