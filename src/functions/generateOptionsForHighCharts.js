45001b9d0aaac6b09882bb795370a4c0d78fcc8d
addania <mirka.schw@gmail.com>
2020-01-14 22:28:56 +0100

    shortening Highcharts Options code further

--- src/functions/generateOptionsForHighCharts.js
+++ src/functions/generateOptionsForHighCharts.js
@@ -6,19 +6,21 @@ import { generateDataSetForHighCharts } from "./generateDataSetForHighCharts";
 export function generateOptionsForHighCharts(data, filter, apply) {
   // As input receives data, filter and condition whether to apply filter or not.
   // Outputs options for HighchartsReact to display either filtered data or entire data set.
-  let message = "";
-  let aggregatedData;
   if (apply) {
-    aggregatedData = calculateAggregates(filterArray(filter, data));
-    message = generateTitleForHighCharts(filter);
+    const aggregatedData = calculateAggregates(filterArray(filter, data));
+    return generateDataSetForHighCharts(
+      aggregatedData.impressions,
+      aggregatedData.clicks,
+      aggregatedData.dates,
+      generateTitleForHighCharts(filter)
+    );
   } else {
-    aggregatedData = calculateAggregates(data);
-    message = "All Datasources; All Campaigns";
+    const aggregatedData = calculateAggregates(data);
+    return generateDataSetForHighCharts(
+      aggregatedData.impressions,
+      aggregatedData.clicks,
+      aggregatedData.dates,
+      "All Datasources; All Campaigns"
+    );
   }
-  return generateDataSetForHighCharts(
-    aggregatedData.impressions,
-    aggregatedData.clicks,
-    aggregatedData.dates,
-    message
-  );
 }
