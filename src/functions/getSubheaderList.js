0a8d343073dcc9a0a4d64e74479e47ad58a752ec
addania <mirka.schw@gmail.com>
2020-01-16 16:34:36 +0100

    rename subheader list generating functions

--- src/functions/getSubheaderList.js
+++ src/functions/getSubheaderList.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { List } from "../components/List.js";
 
-export function subheaderHelperFunction(
-  dimensions,
-  dimensionsLength,
-  italicsInput
-) {
-  const message = "Select zero to N ";
+export function getSubheaderList(dimensions, dimensionsCount, italicsInput) {
   const listItems = [];
-  for (let listItem = 0; listItem < dimensionsLength; listItem++) {
+  for (let listItem = 0; listItem < dimensionsCount; listItem++) {
     listItems.push(
       <List
-        message={message}
+        message="Select zero to N "
         columns={dimensions[listItem]}
         styling={italicsInput}
         key={listItem}
