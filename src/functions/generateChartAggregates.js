6f9774f79a17dbb131040c17b7f93a5fff36630a
addania <mirka.schw@gmail.com>
2020-02-04 07:46:28 +0100

    GithubIssue03_Functional_Programming

--- src/functions/generateChartAggregates.js
+++ src/functions/generateChartAggregates.js
@@ -1,6 +1,6 @@
 export const generateChartAggregates = (groupByDate, metric) =>
-  // As input receives data, filter and condition whether to apply filter or not.
-  // Outputs options for HighchartsReact to display either filtered data or entire data set.
+  // As input receives dataset grouped by day and name of metric.
+  // Outputs array of unique dates and aggregated data of that metric.
   Object.keys(groupByDate).map(item => {
     const sum = groupByDate[item].reduce(
       (accumulator, object) => accumulator + object[metric],
