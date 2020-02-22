f3dded898cf63410144b19f87319aa5736f15e4a
addania <mirka.schw@gmail.com>
2020-02-22 10:24:04 +0100

    deleting external files for parseData helper functions
    as all helper functions used in parseData function were moved from external files to the parseData.js also the external files ned to be deleted as they are now redundant

--- src/functions/getDimensions.js
+++
@@ -1,4 +0,0 @@
-export const getDimensions = (inputData, inputColumns) =>
-  // Receives an array of objects as inputData and array of unique keys as inputColumns.
-  // Outputs array of column names which holds string values (excluding date formats).
-  inputColumns.filter(column => typeof inputData[column] === "string");
--- src/functions/sortArray.js
+++
@@ -1,5 +0,0 @@
-import _ from "lodash";
-
-export const sortArray = input =>
-  // Receives an array of objects as input and sorts the entries based on Date, Datasource and Campaign as output.
-  _.sortBy(input, ["date", "datasource", "campaign"]);
