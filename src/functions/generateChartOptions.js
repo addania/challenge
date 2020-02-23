2d181a672cadca0fe9d192cd50346fe405f92d1c
addania <mirka.schw@gmail.com>
2020-02-23 09:48:57 +0100

    migrate individual functions and test for generateChartOptions.js
    Accoriding to colocation principles, also all individual functions and tests for generateChartOptions.js were migrated to its own file as those are only used there. All individual external functions and test files were removed.

--- src/functions/generateChartOptions.js
+++ src/functions/generateChartOptions.js
@@ -1,7 +1,5 @@
-import { filterArray } from "./filterArray";
-import { aggregate } from "./aggregate";
-import { getChartTitle } from "./getChartTitle";
-import { getChartData } from "./getChartData";
+import _ from "lodash";
+import Highcharts from "highcharts";
 
 export const generateChartOptions = (data, filter, apply) => {
   // As input receives data, filter and condition whether to apply filter or not.
@@ -20,3 +18,148 @@ export const generateChartOptions = (data, filter, apply) => {
     );
   }
 };
+
+export const filterArray = (filter, data) => {
+  // Receives data for filtering and filters in order to calculate subset of data which was filtered.
+  // Outputs filtered array.
+  if (!(Object.entries(filter).length === 0 && filter.constructor === Object)) {
+    if (filter.datasource === undefined || filter.datasource.length === 0) {
+      return data.filter(item => filter.campaign.includes(item.campaign));
+    } else if (filter.campaign === undefined || filter.campaign.length === 0) {
+      return data.filter(item => filter.datasource.includes(item.datasource));
+    } else {
+      return data.filter(
+        item =>
+          filter.datasource.includes(item.datasource) &&
+          filter.campaign.includes(item.campaign)
+      );
+    }
+  }
+};
+
+export const aggregate = array => {
+  // Receives an array of data as input and outputs an object of data aggregated per date.
+  // First key is data series for Highcharts of aggregated impressions.
+  // Second key is data series for Highcharts of aggregated clicks.
+  const groupByDate = _.groupBy(array, "date");
+  return {
+    impressions: {
+      type: "line",
+      name: "Impressions",
+      yAxis: 1,
+      data: generateChartAggregates(groupByDate, "impressions")
+    },
+    clicks: {
+      type: "line",
+      name: "Clicks",
+      data: generateChartAggregates(groupByDate, "clicks")
+    }
+  };
+};
+
+export const generateChartAggregates = (groupByDate, metric) =>
+  // As input receives dataset grouped by day and name of metric.
+  // Outputs array of unique dates and aggregated data of that metric.
+  Object.keys(groupByDate).map(item => {
+    const sum = groupByDate[item].reduce(
+      (accumulator, object) => accumulator + object[metric],
+      0
+    );
+    return [parseInt(item), sum];
+  });
+
+export const getChartTitle = filter => {
+  // Receives filter as input and generates corresponding message
+  if (filter["campaign"] === undefined && filter["datasource"] !== undefined) {
+    return "Datasource " + getTitleList(filter.datasource) + "; All Campaigns";
+  } else if (
+    filter["datasource"] === undefined &&
+    filter["campaign"] !== undefined
+  ) {
+    return "All Datasources; Campaign " + getTitleList(filter.campaign);
+  } else if (
+    filter["campaign"] === undefined &&
+    filter["datasource"] === undefined
+  ) {
+    return "All Datasources; All Campaigns";
+  } else {
+    return (
+      "Datasource " +
+      getTitleList(filter.datasource) +
+      "; Campaign " +
+      getTitleList(filter.campaign)
+    );
+  }
+};
+
+export const getTitleList = filterItem => {
+  // Receives an array of filter values. Generates a string of maximum 3 values
+  // if multiple filter values are selected.
+  const valuesArray = filterItem
+    .slice(0, 3)
+    .map(item => JSON.stringify(item) + " and ");
+  if (filterItem.length <= 3) {
+    return valuesArray.join("").slice(0, -4);
+  } else {
+    return valuesArray.join("").slice(0, -5) + ", etc.";
+  }
+};
+
+export const getChartData = (impressions, clicks, title) =>
+  // Recives series of impressions, series of clicks and title message for Highcharts as input.
+  // Outputs options to visualize data on a HighchartsReact component.
+  ({
+    chart: {
+      type: "spline",
+      zoomType: "x"
+    },
+    title: {
+      text: title,
+      align: "left"
+    },
+    xAxis: {
+      type: "datetime",
+      dateTimeLabelFormats: {
+        day: "%e. %b"
+      }
+    },
+    yAxis: [
+      {
+        // Primary yAxis
+        labels: {
+          format: "{value}",
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        title: {
+          text: "Clicks",
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        opposite: false
+      },
+      {
+        // Secondary yAxis
+        gridLineWidth: 0,
+        title: {
+          text: "Impressions",
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        },
+        labels: {
+          format: "{value}",
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        },
+        opposite: true
+      }
+    ],
+    legend: {
+      enabled: true
+    },
+    series: [clicks, impressions]
+  });
--- src/functions/tests/generateChartOptions.test.js
+++ src/functions/tests/generateChartOptions.test.js
@@ -1,5 +1,14 @@
-import { generateChartOptions } from "../functions/generateChartOptions";
+import {
+  generateChartOptions,
+  filterArray,
+  aggregate,
+  generateChartAggregates,
+  getChartTitle,
+  getTitleList,
+  getChartData
+} from "../generateChartOptions";
 import Highcharts from "highcharts";
+import _ from "lodash";
 
 test("calculate Options", () => {
   expect(
@@ -196,6 +205,235 @@ test("calculate Options", () => {
         name: "Impressions",
         yAxis: 1,
         data: [[1546300800000, 766606]]
+      }
+    ]
+  });
+});
+
+test("filter Array", () => {
+  expect(
+    filterArray(
+      {
+        datasource: ["Facebook Ads", "Google Adwords"],
+        campaign: ["Like Ads"]
+      },
+      [
+        {
+          date: "01. Jan",
+          datasource: "Facebook Ads",
+          campaign: "Like Ads",
+          clicks: 274,
+          impressions: 1979
+        },
+        {
+          date: "01. Jan",
+          datasource: "Facebook Ads",
+          campaign: "Offer Campaigns - Conversions",
+          clicks: 10245,
+          impressions: 764627
+        },
+        {
+          date: "01. Jan",
+          datasource: "Google Adwords",
+          campaign: "B2B - Leads",
+          clicks: 7,
+          impressions: 444
+        }
+      ]
+    )
+  ).toStrictEqual([
+    {
+      date: "01. Jan",
+      datasource: "Facebook Ads",
+      campaign: "Like Ads",
+      clicks: 274,
+      impressions: 1979
+    }
+  ]);
+});
+
+test("aggregate data", () => {
+  expect(
+    aggregate([
+      {
+        date: "01. Jan",
+        datasource: "Facebook Ads",
+        campaign: "Like Ads",
+        clicks: 274,
+        impressions: 1979
+      },
+      {
+        date: "01. Jan",
+        datasource: "Facebook Ads",
+        campaign: "Offer Campaigns - Conversions",
+        clicks: 10245,
+        impressions: 764627
+      },
+      {
+        date: "02. Jan",
+        datasource: "Google Adwords",
+        campaign: "B2B - Leads",
+        clicks: 7,
+        impressions: 444
+      }
+    ])
+  ).toStrictEqual({
+    impressions: [766606, 444],
+    clicks: [10519, 7],
+    dates: ["01. Jan", "02. Jan"]
+  });
+});
+
+test("generate chart aggregates", () => {
+  const data = {
+    1548979200000: [
+      {
+        date: 1548979200000,
+        datasource: "Facebook Ads",
+        campaign: "Like Ads",
+        clicks: 274,
+        impressions: 1979
+      },
+      {
+        date: 1548979200000,
+        datasource: "Facebook Ads",
+        campaign: "Offer Campaigns - Conversions",
+        clicks: 10245,
+        impressions: 764627
+      }
+    ],
+    1546300800000: [
+      {
+        date: 1546300800000,
+        datasource: "Google Adwords",
+        campaign: "B2B - Leads",
+        clicks: 7,
+        impressions: 444
+      }
+    ]
+  };
+
+  expect(generateChartAggregates(data, "impressions")).toStrictEqual([
+    [1548979200000, 766606],
+    [1546300800000, 444]
+  ]);
+
+  expect(generateChartAggregates(data, "clicks")).toStrictEqual([
+    [1548979200000, 10519],
+    [1546300800000, 7]
+  ]);
+});
+
+test("generate message", () => {
+  expect(
+    getChartTitle({
+      datasource: ["Facebook Ads", "Google Adwords"],
+      campaign: ["Like Ads"]
+    })
+  ).toStrictEqual(
+    'Datasource "Facebook Ads" and "Google Adwords" ; Campaign "Like Ads" '
+  );
+});
+
+test("getTitleList", () => {
+  expect(getTitleList(["Facebook Ads", "Google Adwords"])).toStrictEqual(
+    '"Facebook Ads" and "Google Adwords" '
+  );
+});
+
+test("generate Options", () => {
+  expect(
+    getChartData(
+      {
+        type: "line",
+        name: "Impressions",
+        yAxis: 1,
+        data: [
+          [1546300800000, 1724052],
+          [1546387200000, 1955809]
+        ]
+      },
+      {
+        type: "line",
+        name: "Clicks",
+        data: [
+          [1546300800000, 24091],
+          [1546387200000, 19479]
+        ]
+      },
+      "All Datasources; All Campaigns"
+    )
+  ).toStrictEqual({
+    chart: {
+      type: "spline",
+      zoomType: "x"
+    },
+    title: {
+      text: "All Datasources; All Campaigns",
+      align: "left"
+    },
+    xAxis: {
+      type: "datetime",
+      dateTimeLabelFormats: {
+        day: "%e. %b"
+      }
+    },
+    yAxis: [
+      {
+        // Primary yAxis
+        labels: {
+          format: "{value}",
+
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        title: {
+          text: "Clicks",
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        opposite: false
+      },
+      {
+        // Secondary yAxis
+        gridLineWidth: 0,
+        title: {
+          text: "Impressions",
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        },
+        labels: {
+          format: "{value}",
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        },
+        opposite: true
+      }
+    ],
+    legend: {
+      enabled: true
+    },
+    series: [
+      {
+        type: "line",
+        name: "Clicks",
+        data: [
+          [1546300800000, 24091],
+          [1546387200000, 19479]
+        ]
+      },
+      {
+        type: "line",
+        name: "Impressions",
+        yAxis: 1,
+        data: [
+          [1546300800000, 1724052],
+          [1546387200000, 1955809]
+        ]
       }
     ]
   });
